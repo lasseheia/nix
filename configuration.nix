86acd0dec2622730fd43a5ea7bb6e95b2910c3fb
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-11-16 17:02:19 +0100

    feat: set locale time locale to norwegian

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,13 @@
   };
   
   time.timeZone = "Europe/Oslo";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "nb_NO.UTF-8";
+    };
+  };
+  
   console = {
     earlySetup = true;
     keyMap = "no";
