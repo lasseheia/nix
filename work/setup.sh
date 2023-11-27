5faf48cb5e2667849eee22d8d60ca0941c8ab31e
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-11-27 20:06:24 +0100

    feat: add setup script
    Used to prepare a machine for nixos installation.

---
+++ work/setup.sh
@@ -0,0 +1,62 @@
+# Remote installation
+## Set up wifi connection
+sudo systemctl start wpa_supplicant
+wpa_cli
+add_network
+set_network 0 ssid ""
+set_network 0 psk ""
+set_network 0 key_mgmt WPA-PSK
+enable_network 0
+
+## Set user password
+passwd
+
+# Partitioning
+## Wipe the current disk
+sudo blkdiscard /dev/nvme0n1
+
+## Partition the disk
+sudo parted /dev/nvme0n1 --script -- \
+  mklabel gpt \
+  mkpart ESP fat32 1MiB 1GiB \
+  set 1 esp on \
+  mkpart primary 1GiB 100%
+
+# Format and encrypt the partitions
+## Format the EFI partition and label it
+sudo mkfs.fat -F 32 /dev/nvme0n1p1
+
+## Set up LUKS2 encryption for the root partition
+sudo cryptsetup luksFormat --type luks2 /dev/nvme0n1p2
+sudo cryptsetup open /dev/nvme0n1p2 nixos-enc
+
+# Set up LVM
+## Create a physical volume on top of the opened LUKS container
+sudo pvcreate /dev/mapper/nixos-enc
+
+## Create a volume group
+sudo vgcreate vg /dev/mapper/nixos-enc
+
+## Create logical volumes
+sudo lvcreate -L 50G vg -n root
+sudo lvcreate -l +100%FREE vg -n home
+
+## Format logical volumes
+sudo mkfs.ext4 /dev/vg/root
+sudo mkfs.ext4 /dev/vg/home
+
+## Mount the logical volumes
+sudo mount /dev/vg/root /mnt
+sudo mkdir /mnt/home
+sudo mount /dev/vg/home /mnt/home
+sudo mkdir /mnt/boot
+sudo mount /dev/nvme0n1p1 /mnt/boot
+
+# Configure NixOS
+## Clone GitHub repository
+nix-env -iA nixos.git nixos.gh
+gh auth login
+gh repo clone lasseheia/nix
+
+## Install nixos using flake
+sudo nixos-install --flake .#nixos-orange --impure
