305bfded923f7975f0db6a5c6fab50adb9babc2a
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-12-03 19:38:38 +0100

    feat(desktop): enable automatic store optimization

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -3,6 +3,7 @@
 {
   system.stateVersion = "23.05";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
   nixpkgs.config.allowUnfree = true;
 
   services.lvm.enable = true;
