692ee7efcf4e5f94b9aaee1dd087934d96a01c40
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-12-06 23:29:55 +0100

    fix(wsl): set nixpkgs.hostPlatform
    https://github.com/lasseheia/nix/actions/runs/7120973769:
    > error: Neither nixpkgs.hostPlatform nor the legacy option nixpkgs.system has been set. You can set nixpkgs.hostPlatform in hardware-configuration.nix by re-running a recent version of nixos-generate-config. The option nixpkgs.system is still fully supported for NixOS 22.05 interoperability, but will be deprecated in the future, so we recommend to set nixpkgs.hostPlatform.

--- nixos/hosts/wsl/default.nix
+++ nixos/hosts/wsl/default.nix
@@ -1,4 +1,6 @@
 {
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
   system.stateVersion = "23.05";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
