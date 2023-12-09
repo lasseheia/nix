182b7f6985795324c5ac29da670561683de2ee55
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-12-09 17:22:46 +0100

    refactor(nixos): move filesystem and luks configuration to base module

--- nixos/hosts/desktop/default.nix
+++ nixos/hosts/desktop/default.nix
@@ -3,6 +3,29 @@
     ./hardware-configuration.nix
   ];
 
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/vg/root";
+      fsType = "ext4";
+    };
+    "/home" = {
+      device = "/dev/vg/home";
+      fsType = "ext4";
+    };
+  };
+
   xdg = {
     portal = {
       enable = true;
--- nixos/hosts/desktop/hardware-configuration.nix
+++ nixos/hosts/desktop/hardware-configuration.nix
@@ -10,30 +10,6 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  boot.initrd.luks.devices = {
-    root = {
-      device = "/dev/nvme0n1p2";
-      preLVM = true;
-      allowDiscards = true;
-    };
-  };
-  
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/nvme0n1p1";
-      fsType = "vfat";
-    };
-    "/" = {
-      device = "/dev/vg/root";
-      fsType = "ext4";
-    };
-    "/home" = {
-      device = "/dev/vg/home";
-      fsType = "ext4";
-    };
-  };
-
-
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
