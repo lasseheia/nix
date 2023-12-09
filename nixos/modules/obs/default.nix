a3742fc98c57f7fd676da3f5c90138553de87735
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-12-09 21:37:57 +0100

    feat(obs): configure virtualcam for obs

--- nixos/modules/obs/default.nix
+++ nixos/modules/obs/default.nix
@@ -5,8 +5,11 @@
     obs-studio
   ];
 
-  boot = {
-    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    kernelModules = [ "v4l2loopback" ];
-  };
+  # https://nixos.wiki/wiki/OBS_Studio
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
