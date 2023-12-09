68068dd2d2a0e250286102d7960f7c770a6426d6
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-12-09 20:46:27 +0100

    style: don't expand xdg

--- nixos/modules/hyprland/default.nix
+++ nixos/modules/hyprland/default.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
 
 {
-  xdg = {
-    portal = {
-      enable = true;
-      wlr.enable = true;
-    };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
 
   services = {
