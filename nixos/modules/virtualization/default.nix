050956e8b7fb36b36e6e264a6ee97f3433ca0464
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-01-13 13:33:51 +0100

    feat(virtualization): add docker and group assignments

--- nixos/modules/virtualization/default.nix
+++ nixos/modules/virtualization/default.nix
@@ -1,4 +1,22 @@
+{ pkgs, ... }:
+
 {
-  virtualisation.libvirtd.enable = true;
+  users.users.lasse.extraGroups = [
+    "libvirtd"
+    "docker"
+  ];
+
+  virtualisation.docker.enable = true;
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+    };
+  };
+
   programs.virt-manager.enable = true;
+  services.spice-vdagentd.enable = true;
 }
