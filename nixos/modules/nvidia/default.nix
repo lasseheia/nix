608c25d302b40b5d58016bfde4c0093ba9a6d9c0
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-01-15 09:40:43 +0100

    feat(nvidia): use beta driver for nvidia

--- nixos/modules/nvidia/default.nix
+++ nixos/modules/nvidia/default.nix
@@ -33,7 +33,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # https://wiki.hyprland.org/Nvidia/
