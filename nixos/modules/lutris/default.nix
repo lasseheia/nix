512c054210ad0ed6bd30f1274e0553668eea4e86
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-03-03 15:58:53 +0100

    chore: remove unused modules and delete nixos modules folder

--- nixos/modules/lutris/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  environment.systemPackages = with pkgs; [
-    lutris
-    wine # https://github.com/lutris/docs/blob/master/WineDependencies.md
-  ];
-  hardware.opengl.driSupport32Bit = true; # https://nixos.wiki/wiki/Lutris - Also enabled in `modules/steam.nix`
-}
--- nixos/modules/steam/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  # https://nixos.wiki/wiki/Steam
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
-}
