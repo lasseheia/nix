1313b0f772cd02707628098c51bd09fb61390831
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-03-03 01:11:14 +0100

    refactor(modules/hyprland): move nixos configuration to default.nix

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -4,6 +4,35 @@
 }:
 
 {
-  imports = [ ./nixos.nix ];
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  };
+
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+  };
+
+  # https://wiki.hyprland.org/Nix/Hyprland-on-NixOS/
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    enableNvidiaPatches = true;
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # https://wiki.hyprland.org/Useful-Utilities/Screen-Sharing/#prerequisites
+  environment.systemPackages = [
+    pkgs.xwaylandvideobridge
+  ];
+
+  services.blueman.enable = true;
+
   home-manager.users.lasse = ./home-manager.nix;
 }
