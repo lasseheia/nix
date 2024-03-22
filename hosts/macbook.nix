24894d2d3f0482611cdb0418b76275a60ae024ef
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-03-22 23:43:36 +0100

    :wrench: (hosts/macbook): Allow unfree packages

--- hosts/macbook.nix
+++ hosts/macbook.nix
@@ -9,9 +9,9 @@
     inputs.home-manager.darwinModules.default
   ];
 
-
   system.stateVersion = 4;
   nix.package = pkgs.nix;
+  nixpkgs.config.allowUnfree = true;
   services.nix-daemon.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
 
