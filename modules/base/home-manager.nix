f8395d7ad68848b31e2bfcc89f76f90366112220
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-10-27 01:24:48 +0200

    :fire: (modules/base/home-manager): Remove unused packages

--- modules/base/home-manager.nix
+++ modules/base/home-manager.nix
@@ -1,16 +1,7 @@
-{ pkgs, ... }:
-
 {
   programs.home-manager.enable = true;
 
   home.stateVersion = "23.05";
   home.username = "lasse";
   home.homeDirectory = "/home/lasse";
-
-  home.packages = with pkgs; [
-    signal-desktop
-    ledger-live-desktop
-    yubikey-manager-qt
-    mullvad
-  ];
 }
