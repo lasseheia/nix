eef1a635157761af7ff0938042b9d3927a7fd43d
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-11-05 21:31:59 +0100

    :wrench: (hosts/desktop): Add some host specific packages

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -1,10 +1,17 @@
 {
+  pkgs,
   inputs,
   config,
   lib,
   ...
 }:
 
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    inherit (config.nixpkgs) system;
+    config.allowUnfree = true;
+  };
+in
 {
   boot.initrd.availableKernelModules = [
     "nvme"
@@ -27,5 +34,13 @@
     ../modules/neovim
     ../modules/git
     ../modules/hyprland
+  ];
+
+  environment.systemPackages = [
+    pkgs.prusa-slicer
+    pkgs.openscad
+    pkgs-unstable.azuredatastudio
+    pkgs-unstable.rustdesk-flutter
+    pkgs-unstable.rpi-imager
   ];
 }
