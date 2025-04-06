bb84208f4cb40d70dc24bcec84648d945823e6df
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2025-04-06 14:15:34 +0200

    :bug: (hosts/laptop): Add LUKS configuration for root and home partitions
    This configuration was removed from the nixos module and must be added on the hosts level now.

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -17,6 +17,31 @@ in
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
 
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/vg/root";
+      fsType = "ext4";
+    };
+    "/home" = {
+      device = "/dev/vg/home";
+      fsType = "ext4";
+    };
+  };
+
+  swapDevices = [ ];
+
   imports = [
     inputs.nixos-hardware.nixosModules.dell-latitude-7490
     ../../modules/nixos
