840ed709c1b07483ba796bc68294bd124f7ef3ca
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2025-04-11 21:48:31 +0200

    last

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,11 +1,81 @@
+{ inputs, ... }:
+
 {
   imports = [
     ../../modules/nixos
-    ../../modules/zfs
+    inputs.disko.nixosModules.disko
   ];
 
   users.users.lasse.isNormalUser = true;
 
   users.users.lasse.group = "lasse";
   users.groups.lasse = {};
+
+  networking.hostId = "b648d918";
+
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "nofail" ];  # Continue boot even if /boot fails to mount
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          mountpoint = "none";  # Disable automatic mounting; set mountpoints per dataset below
+          compression = "zstd"; # Efficient, modern compression
+        };
+        options.ashift = "12";  # Ensures proper 4K alignment for NVMe/SSD devices
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+          };
+          "root/home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+          };
+          "root/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+          };
+          "root/swap" = {
+            type = "zfs_volume";
+            size = "2G";
+            content = {
+              type = "swap";
+            };
+            options = {
+              volblocksize = "4096";
+            };
+          };
+        };
+      };
+    };
+  };
 }
