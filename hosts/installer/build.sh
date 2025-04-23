168325a31a5087582505e849c0c77f4cfb817880
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2025-04-23 19:17:01 +0200

    :sparkles: (hosts): Set up installer for NixOS installation using nixos-anywhere

---
+++ hosts/installer/build.sh
@@ -0,0 +1,1 @@
+nix build .#nixosConfigurations.installer.config.system.build.isoImage
--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -5,15 +5,22 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
-  environment.etc."install.sh" = {
-    source = ./scripts/install.sh;
-    mode = "0755";
-  };
-
-  console.keyMap = "no";
+  nix.settings.experimental-features = "nix-command flakes";
 
   networking.wireless = {
     enable = false;
     iwd.enable = true;
   };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
+
+  users.users.nixos.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8V+W2mKUj8QpWJe5N8Z6zrekUISHwdXy6vp4nkte4l"
+  ];
+
+  console.keyMap = "no";
 }
