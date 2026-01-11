afb18950ca1d20529f1aa5c062b2849f31a7a38e
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-01-11 15:19:52 +0100

    feat(hosts/server/default.nix): Enable flakes and nix-command
    To be able to use devshell on the host.

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -2,6 +2,7 @@ let
   ssh_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8V+W2mKUj8QpWJe5N8Z6zrekUISHwdXy6vp4nkte4l" ];
 in
 {
+  nix.settings.experimental-features = "nix-command flakes";
 
   system.stateVersion = "24.11";
   console.keyMap = "no";
