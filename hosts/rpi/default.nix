8373d7668ead23157d5f324f8dee7ec4fc6884b4
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-01-27 23:26:35 +0100

    feat(hosts/rpi/default.nix): add SSH key authentication and disable password login

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -1,3 +1,6 @@
+let
+  ssh_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8V+W2mKUj8QpWJe5N8Z6zrekUISHwdXy6vp4nkte4l" ];
+in
 {
   networking = {
     hostName = "rpi";
@@ -9,11 +12,22 @@
     };
   };
 
-  services.openssh.enable = true;
-  console.keyMap = "no";
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
 
-  users.users.lasse = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
+  users.users = {
+    root = {
+      isNormalUser = false;
+      openssh.authorizedKeys.keys = ssh_keys;
+    };
+    lasse = {
+      isNormalUser = true;
+      home = "/home/lasse";
+      openssh.authorizedKeys.keys = ssh_keys;
+      extraGroups  = [ "wheel" ];
+    };
   };
 }
