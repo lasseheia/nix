aa21bf25e30dcfa82f9bd133ed5fda237e80a195
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-01-27 23:26:56 +0100

    chore(hosts/rpi/hardware-configuration.nix): bump stateVersion to 25.05
    No need for backwards compatibility on this new install.

--- hosts/rpi/hardware-configuration.nix
+++ hosts/rpi/hardware-configuration.nix
@@ -11,7 +11,7 @@
   };
 
   hardware.enableRedistributableFirmware = true;
-  system.stateVersion = "24.05";
+  system.stateVersion = "25.05";
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
