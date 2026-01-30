ab9147834f3cca798c1c34b4f34f045570f1a444
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2026-01-31 00:14:37 +0100

    feat(hosts/laptop/hardware-configuration.nix): enable blueman

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -10,6 +10,8 @@
   # https://nixos.wiki/wiki/Bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
   networking.hostName = "laptop";
 
   boot.loader = {
